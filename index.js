5ec0b73ebd7abc6ee1d19955b912c70b0e11265c
emilyjcastillomx <a00839559@tec.mx>
2025-05-15 10:41:47 -0600

    First commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use("/", express.static("public"));
 
-const port = 3200;
+const port = 2500;
 
 app.listen(port, console.log("http://localhost:"+port));
+
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -7,13 +7,24 @@ const boton = document.getElementById("btnLogin");
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 
-const login = () => {
-    const user = username.value;
-    if (user == "Emily Castillo") {
-        sessionStorage.setItem("name", "Emily Castillo");
-        window.location.href = "pages/profile.html";
-    } else {
-        alert("Por favor ingresa usuario y contraseña validos.");
+const login = async () => {
+    // validar credenciales
+    const credentials = {username: username.value, password: password.value};
+    const data = await fetch("http://localhost:2700/login", {
+        method: "POST",
+        headers:{"content-type": "application/json" }, 
+        body: JSON.stringify(credentials),
+    })
+    const res = await data.json();
+    // console.log(username.value + " " + password.value);
+    // si es login correcto
+    if (res.isLogin === true){
+        sessionStorage.setItem("name", res.user.name);
+        sessionStorage.setItem("id", res.user.id);
+        window.location = "./pages/profile.html";
+    } else{
+        // si el login es incorrecto
+        alert("Credenciales Incorrectas");
     }
 };
 boton.addEventListener("click", login);
